6b8226cee216f800fc463640e3a1ed51daaf06b8
leekyuchan0215 <20212977@office.deu.ac.kr>
2024-11-13 23:28:17 +0900

    [Add] test
    ... 없애고 !

--- src/main/java/deu/hms/HMs.java
+++ src/main/java/deu/hms/HMs.java
@@ -11,6 +11,6 @@ package deu.hms;
 public class HMs {
 
     public static void main(String[] args) {
-        System.out.println("Hello World.......");
+        System.out.println("Hello World!");
     }
 }
